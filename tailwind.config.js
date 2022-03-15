6cf9a8f7c416491980a11d054f34f23a19149a49
marin <marin.nicolas.ludovic@gmail.com>
2022-03-15 11:23:30 +0100

    design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,28 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-grayish-blue': '#323a49',
+        'grayish-blue': '#4e5d73',
+        'dark-blue':'#1f2632',
+        'neon-green':'#52ffa8',
+        'light-cyan' : '#cee3e9'
+      },
+      borderRadius: {
+        'large': '20px',
+      },
+      fontFamily: {
+        'manrope': ['Manrope']
+      },
+      fontSize: {
+        '28': '28px',
+      },
+      height: {
+        '0.1': '1px',
+      }
+  
+    },
   },
   plugins: [],
 }
